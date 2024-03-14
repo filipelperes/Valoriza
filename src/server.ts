d36b9208b9e7352b4c91faba0789c04c946b5a90
Filipe Lago Peres <filipelperes@gmail.com>
2024-03-13 21:59:03 -0300

    lib for front end

--- src/server.ts
+++ src/server.ts
@@ -4,10 +4,12 @@ import "express-async-errors";
 import { router } from "./routes";
 
 import "./database";
+import cors from "cors";
 
 const app = express();
 const port = process.env.PORT ?? 3000;
 
+app.use(cors());
 app.use(express.json());
 app.use(router);
 app.use((err: Error, request: Request, response: Response, next: NextFunction) => {
